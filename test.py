07e37a594706ba5f3f1af65352a4536be1b307af
Md Emanuel Hossain <mdemanuelh7@gmail.com>
2021-08-06 00:57:11 +0100

    Fixed speed

--- test.py
+++
@@ -1,7 +0,0 @@
-arr = [5,1,4,2,8]
-
-def algorithm(arr):
-        for i in range(len(arr)):
-            for j in range(len(arr)-i-1):
-                if arr[j] > arr[j+1]:
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
